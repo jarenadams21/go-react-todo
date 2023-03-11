d3f7ba98e5cf4d990cbe4360feb1e18cc36faf9d
jarenadams21 <jarenadams3@gmail.com>
2023-03-10 19:24:06 -0500

    init-base-app

---
+++ client/src/App.tsx
@@ -0,0 +1,66 @@
+import { Box, List, ThemeIcon } from '@mantine/core'
+import { CheckCircleFillIcon } from '@primer/octicons-react';
+import useSWR from "swr";
+import './App.css'
+import AddTodo from "./components/AddTodo"
+
+export interface Todo {
+  id: number
+  title: string
+  body: string
+  done: boolean
+}
+
+export const ENDPOINT = 'http://localhost:4000'
+
+// SWR requires us to create our own fetcher
+const fetcher = (url: string) => 
+fetch(`${ENDPOINT}/${url}`).then((r) => r.json());
+
+function App() {
+
+  // useSWR returns data and a mutate object that lets us update state
+  const {data, mutate} = useSWR<Todo[]>('api/todos', fetcher)
+
+  async function markTodoAsDone(id: number) {
+
+    const updated = await fetch(`${ENDPOINT}/api/todos/${id}/done`, {
+      method: 'PATCH',
+    }).then((r) => r.json());
+
+    mutate(updated)
+  }
+
+  return <Box
+  sx={(theme) => ({
+    padding: "2rem",
+    width: '100%',
+    maxWidth: '40rem',
+    margin: '0 auto'
+  })}
+  >
+    <List spacing="s" size="sm" mb={12} center>
+      {data?.map((todo) => {
+      return (
+        <List.Item
+        onClick={() => markTodoAsDone(todo.id)} 
+        key={`todo_list__${todo.id}`}
+        icon={
+          todo.done ? (<ThemeIcon color="green" size={24} radius="xl">
+            <CheckCircleFillIcon size={20}/>
+          </ThemeIcon>) : (<ThemeIcon color="gray" size={24} radius="xl">
+            <CheckCircleFillIcon size={20}/>
+          </ThemeIcon>
+          )
+        }
+          >
+          {todo.title} </List.Item>
+      )
+  })}
+   </List>
+
+  <AddTodo mutate={mutate}/>
+  </Box>;
+}
+
+export default App
---
+++ server/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+type Todo struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Done  bool   `json:"done"`
+	Body  string `json:"body"`
+}
+
+func main() {
+	fmt.Print("Hello world")
+
+	// Infer the type from whatever New() returns
+	// It returns a pointer to an app instance of type App struct
+	// Server creation
+	app := fiber.New()
+
+	// Avoid CORS issues
+	// Define our type for the config
+	// Allow origins (whitlisted urls)
+	// Allow headers
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://127.0.0.1:5173",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+
+	todos := []Todo{}
+
+	// Could also return an error (error syntax)
+	app.Get("/healthcheck", func(c *fiber.Ctx) error {
+		return c.SendString("OK")
+	})
+
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+
+		// Return errors
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+
+		// give todo an ID
+		todo.ID = len(todos) + 1
+
+		// Append new todo to array of todos
+		todos = append(todos, *todo)
+
+		// Return slice of todos we have
+		return c.JSON(todos)
+	})
+
+	app.Patch("/api/todos/:id/done", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
+
+		if err != nil {
+			return c.Status(401).SendString("Invalid id")
+		}
+
+		// i = index
+		// t = todo
+		for i, t := range todos {
+			if t.ID == id {
+				todos[i].Done = true
+				break
+			}
+
+		}
+
+		return c.JSON(todos)
+	})
+
+	app.Get("/api/todos", func(c *fiber.Ctx) error {
+		return c.JSON(todos)
+	})
+
+	log.Fatal(app.Listen(":4000"))
+}
